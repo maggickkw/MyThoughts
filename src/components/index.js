0eb16d3ff97bf8d4b91718ac7731924a09b1c1cb
Wilberforce Sedem Haibor <wmaggickk@gmail.com>
2023-05-02 03:09:14 +0100

    git fixed firebase

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { Header } from "./Header";
 export { Footer } from "./Footer";
-export { PostCard } from "./PostCard"; 
 export { SkeletonCard } from "./SkeletonCard";
+export { PostCard } from "./PostCard"; 
